91fefc28d758cb832e2132dd06d9d8796dff0d34
Mustafa <124535608+MUChappy@users.noreply.github.com>
2023-09-05 11:30:01 -0400

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 /* script.js */
+
+function getComputerChoice() {
+  /* get random number from range 0-2, then return rock, paper, or scissors based on number */
+    
+  randomChoice = Math.floor(Math.random() * 3);
+
+  switch(randomChoice) {
+    case 0:
+        return "Rock";
+    case 1:
+        return "Paper";
+    case 2:
+        return "Scissors";
+    default:
+        return "";
+  }
+}
