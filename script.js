041a694d2852b43a5fdba29e9ac8dd304469be45
Mustafa <124535608+MUChappy@users.noreply.github.com>
2023-09-05 12:22:22 -0400

    Add 5 round game function game() and gameLoop() function

--- script.js
+++ script.js
@@ -45,42 +45,99 @@ function validatePlayerSelection(playerSelection) {
 }
 
 function battle (computerChoice, playerSelection) {
+    const DRAW = 0;
+    const PLAYERWIN = 1;
+    const PLAYERLOSE = -1;
 
     if (computerChoice === playerSelection) {
         alert("Draw! Both sides picked " + computerChoice);
-        return;
+        return DRAW;
     }
 
     switch (playerSelection) {
         case "rock":
             if (computerChoice === "paper") {
                 alert ("You lose! Paper beats rock!");
+                return PLAYERLOSE;
             }
             else {
                 alert ("You win! Rock beats scissors!");
+                return PLAYERWIN;
             }
             break;
         case "paper":
             if (computerChoice === "scissors") {
                 alert ("You lose! Scissors beats paper!");
+                return PLAYERLOSE;
             }
             else {
                 alert ("You win! Paper beats rock!");
+                return PLAYERWIN;
             }
             break;
         case "scissors":
             if (computerChoice === "rock") {
                 alert("You lose! Rock beats scissors!");
+                return PLAYERLOSE;
             }
             else {
                 alert ("You win! Scissors beats paper!");
+                return PLAYERWIN;
             }
             break;
         default:
             alert ("Nothing beats banana!");
+            return DRAW;
         }
 }
 
-let playerSelection = getPlayerSelection();
-let computerChoice = getComputerChoice();
-battle(computerChoice, playerSelection);
+function game() {
+    const GAMELENGTH = 5;
+    let userScore = 0;
+    let computerScore = 0;
+
+    alert ("Starting 5 round game of rock, paper, scissors");
+
+    for (let gameNum = 0; gameNum < GAMELENGTH; gameNum++) {
+        let playerSelection = getPlayerSelection();
+        let computerChoice = getComputerChoice();
+        let score = battle(computerChoice, playerSelection);
+
+        if (score > 0) {
+            userScore++;
+        }
+        else if (score < 0) {
+            computerScore++;
+        }
+    }
+
+    if (userScore > computerScore) {
+        alert ("Congratulations! You won!");
+    }
+    else if (userScore < computerScore) {
+        alert ("Too bad! You lost!");
+    }
+    else {
+        alert("It's a draw!");
+    }
+}
+
+function gameLoop(){
+    /* main game loop, ask user if they want to play a game, if yes, play, else, stop program */
+    let keepGoing = true;
+    while (keepGoing){
+        let userConfirm = confirm("Would you like to play a game of rock, paper, scissors?");
+
+        if (userConfirm === true) {
+            game();
+        }
+        else {
+            alert ("Goodbye. Thanks for playing.");
+            keepGoing = false;
+        }
+    }
+}
+
+gameLoop();
+
+
